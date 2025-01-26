3994289586246bebd173dcc9ed5907bd3db90640
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-01-26 15:44:37 -0300

    Criando DatabaseHelper e Dao para a classe Usuario.

---
+++ app/src/main/java/br/edu/ifsp/dmo1/pesquisaopiniao/data/database/DatabaseHelper.kt
@@ -0,0 +1,40 @@
+package br.edu.ifsp.dmo1.pesquisaopiniao.data.database
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_KEYS.DB_NAME, null, DATABASE_KEYS.DB_VERSION) {
+
+    object DATABASE_KEYS {
+        const val DB_NAME = "votes_db"
+        const val DB_VERSION = 1
+        const val TB_USER = "tb_user"
+        const val TB_VOTO = "tb_voto"
+        const val USER_COL_PRONTUARIO = "prontuario"
+        const val USER_COL_NOME = "nome"
+        const val VOTO_COL_CODIGO = "codigo"
+        const val VOTO_COL_OPCAO = "opcao"
+    }
+
+    private companion object {
+        const val CREATE_TABLE_USER_V1 =
+            "CREATE TABLE ${DATABASE_KEYS.TB_USER} (${DATABASE_KEYS.USER_COL_PRONTUARIO} TEXT PRIMARY KEY, ${DATABASE_KEYS.USER_COL_NOME} TEXT)"
+        const val CREATE_TABLE_VOTO_V1 =
+            "CREATE TABLE ${DATABASE_KEYS.TB_VOTO} (${DATABASE_KEYS.VOTO_COL_CODIGO} TEXT PRIMARY KEY, ${DATABASE_KEYS.VOTO_COL_OPCAO} TEXT)"
+        const val DROP_TABLE_USER = "DROP TABLE IF EXISTS ${DATABASE_KEYS.TB_USER}"
+        const val DROP_TABLE_VOTO = "DROP TABLE IF EXISTS ${DATABASE_KEYS.TB_VOTO}"
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(CREATE_TABLE_USER_V1)
+        db.execSQL(CREATE_TABLE_VOTO_V1)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL(DROP_TABLE_USER)
+        db.execSQL(DROP_TABLE_VOTO)
+        onCreate(db)
+    }
+}
---
+++ app/src/main/java/br/edu/ifsp/dmo1/pesquisaopiniao/data/database/UserDao.kt
@@ -0,0 +1,63 @@
+package br.edu.ifsp.dmo1.pesquisaopiniao.data.database
+
+import android.content.ContentValues
+import br.edu.ifsp.dmo1.pesquisaopiniao.data.model.User
+
+class UserDao(private val dbHelper: DatabaseHelper) {
+
+    fun insert(user: User){
+        val db = dbHelper.writableDatabase
+        val values = ContentValues().apply {
+            put(DatabaseHelper.DATABASE_KEYS.USER_COL_PRONTUARIO, user.prontuario)
+            put(DatabaseHelper.DATABASE_KEYS.USER_COL_NOME, user.nome)
+        }
+        db.insert(DatabaseHelper.DATABASE_KEYS.TB_USER, null, values)
+    }
+
+    fun getCount(): Int {
+        val count : Int
+        val db = dbHelper.readableDatabase
+
+        val cursor = db.query(
+            DatabaseHelper.DATABASE_KEYS.TB_USER,
+            arrayOf("COUNT(*)"),
+            null,
+            null,
+            null,
+            null,
+            null
+        )
+
+        cursor.use{
+            cursor.moveToNext()
+            count = cursor.getInt(0)
+        }
+        return count
+    }
+
+    fun getByProntuario(prontuario:String): Boolean{
+        val existe: Boolean
+        val db = dbHelper.readableDatabase
+
+        val columns = arrayOf(DatabaseHelper.DATABASE_KEYS.USER_COL_PRONTUARIO)
+
+        val where = "${DatabaseHelper.DATABASE_KEYS.USER_COL_PRONTUARIO} = ?"
+        val whereArgs = arrayOf(prontuario)
+
+        val cursor = db.query(
+            DatabaseHelper.DATABASE_KEYS.TB_USER,
+            columns,
+            where,
+            whereArgs,
+            null,
+            null,
+            null
+        )
+
+        cursor.use{
+            existe = cursor.moveToNext()
+        }
+
+        return existe
+    }
+}
