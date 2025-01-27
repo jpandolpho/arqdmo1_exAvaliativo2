d5b73d2bc6cb9f1e1a63208cf4413cab24af0b01
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-01-27 14:00:36 -0300

    Adicionando geração aleatória de código para o voto.

--- app/src/main/java/br/edu/ifsp/dmo1/pesquisaopiniao/data/model/Voto.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/pesquisaopiniao/data/model/Voto.kt
@@ -1,3 +1,12 @@
 package br.edu.ifsp.dmo1.pesquisaopiniao.data.model
 
-class Voto(val codigo: String, val opcao: String)
+class Voto(val codigo: String, val opcao: String) {
+    companion object {
+        //solução para geração aleatoria encontrada em https://www.baeldung.com/kotlin/random-alphanumeric-string
+        fun generateCode(): String {
+            val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+            val randomCode = List(10) { charPool.random() }.joinToString("")
+            return randomCode
+        }
+    }
+}
