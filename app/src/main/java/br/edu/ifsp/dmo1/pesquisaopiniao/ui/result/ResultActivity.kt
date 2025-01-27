17551c41e5b3b5aea92f5f6b507d0fc71c4150db
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-01-27 17:11:51 -0300

    Implementando lógica da ResultActivity e seu ViewModel. Pequenos ajustes no layout.

--- app/src/main/java/br/edu/ifsp/dmo1/pesquisaopiniao/ui/result/ResultActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/pesquisaopiniao/ui/result/ResultActivity.kt
@@ -1,21 +1,84 @@
 package br.edu.ifsp.dmo1.pesquisaopiniao.ui.result
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.edu.ifsp.dmo1.pesquisaopiniao.R
+import br.edu.ifsp.dmo1.pesquisaopiniao.databinding.ActivityResultBinding
+import br.edu.ifsp.dmo1.pesquisaopiniao.ui.vote.VoteActivity
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityResultBinding
+    private lateinit var viewModel : ResultViewModel
+    private lateinit var adapter : VoteCountAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_result)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(ResultViewModel::class.java)
+
+        setupListeners()
+        setupObservers()
+        verifyBundle()
+    }
+
+    private fun setupListeners() {
+        binding.buttonCheck.setOnClickListener{
+            val codigo = binding.textCodigo.text.toString()
+            viewModel.checkCodigo(codigo)
         }
+
+        binding.buttonVoltar.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun verifyBundle() {
+        if(intent.extras != null){
+            setupRecyclerView()
+            val count = intent.getStringExtra("contagem")
+            val text = binding.voteCount.text.toString()
+            binding.voteCount.text = "$text $count"
+            viewModel.load()
+        }else{
+            binding.resultsTitle.text = "INSIRA SEU CÓDIGO"
+            binding.voteCount.visibility = View.GONE
+            binding.listVotes.visibility = View.GONE
+
+            binding.textlayoutCodigo.visibility = View.VISIBLE
+            binding.buttonCheck.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setupRecyclerView() {
+        adapter = VoteCountAdapter(mutableListOf())
+        binding.listVotes.adapter = adapter
+        binding.listVotes.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun setupObservers() {
+        viewModel.results.observe(this, Observer {
+            adapter.loadData(it)
+        })
+
+        viewModel.opcao.observe(this, Observer {
+            if(it.isEmpty()){
+                Toast.makeText(this,"Código inválido.", Toast.LENGTH_SHORT).show()
+            }else{
+                val mIntent = Intent(this, VoteActivity::class.java)
+                mIntent.putExtra("opcao",it)
+                startActivity(mIntent)
+            }
+        })
     }
 }
---
+++ app/src/main/java/br/edu/ifsp/dmo1/pesquisaopiniao/ui/result/ResultViewModel.kt
@@ -0,0 +1,29 @@
+package br.edu.ifsp.dmo1.pesquisaopiniao.ui.result
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import br.edu.ifsp.dmo1.pesquisaopiniao.data.repository.VotoRepository
+
+class ResultViewModel(application: Application): AndroidViewModel(application) {
+    private var votoRepository: VotoRepository
+
+    private val _results = MutableLiveData<List<Pair<String, Int>>>()
+    val results : LiveData<List<Pair<String, Int>>> = _results
+
+    private val _opcao = MutableLiveData<String>()
+    val opcao : LiveData<String> = _opcao
+
+    init{
+        votoRepository = VotoRepository(application)
+    }
+
+    fun load(){
+        _results.value = votoRepository.getContagemByOpcao()
+    }
+
+    fun checkCodigo(codigo: String) {
+        _opcao.value = votoRepository.getVotoByCodigo(codigo) ?:""
+    }
+}
