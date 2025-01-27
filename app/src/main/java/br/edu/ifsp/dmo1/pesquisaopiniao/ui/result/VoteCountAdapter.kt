0ae240912be6ffc69eff652118380c357301d344
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-01-27 16:29:03 -0300

    Criando e implementando Adapter para a lista de resultados.

---
+++ app/src/main/java/br/edu/ifsp/dmo1/pesquisaopiniao/ui/result/VoteCountAdapter.kt
@@ -0,0 +1,35 @@
+package br.edu.ifsp.dmo1.pesquisaopiniao.ui.result
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.edu.ifsp.dmo1.pesquisaopiniao.R
+import br.edu.ifsp.dmo1.pesquisaopiniao.databinding.ItemListVoteBinding
+
+class VoteCountAdapter(private var dataset: List<Pair<String,Int>>) : RecyclerView.Adapter<VoteCountAdapter.ViewHolder>() {
+
+    fun loadData(data: List<Pair<String,Int>>){
+        dataset = data
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list_vote,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val (opcao,count) = dataset[position]
+        holder.binding.itemOption.setText(opcao)
+        holder.binding.itemCount.setText("${count}")
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val binding: ItemListVoteBinding = ItemListVoteBinding.bind(view)
+    }
+}
